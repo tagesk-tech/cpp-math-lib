fa7c49bfcee1c25b50e8ffc9b419679f96ae7cd0
Tage_skoldefors <116523266+tagesk-tech@users.noreply.github.com>
2025-08-06 12:44:55 +0200

    dsdsfsdfds

---
+++ PDE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>        // for popen, fprintf
+#include <eigen3/Eigen/Dense>
+#include <functional>
+#include <cmath>        // for std::pow
+#include <stdexcept>    // for std::runtime_error
+#include <algorithm>    // for std::swap
+
+
+
+using namespace Eigen;
+using Vec = VectorXd;
+using Mat = MatrixXd;
+
+
